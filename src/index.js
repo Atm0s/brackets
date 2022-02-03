ac9379034474832fdad145e75c7668acab807e24
Atm0s <atm0s@ya.ru>
2022-02-03 12:17:48 +0300

    feat: implement brackets sequence checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const OPEN_BRACKETS = [], BRACKETS_PAIR = {};
+	let stack = [];
+	bracketsConfig.forEach(element => {
+		BRACKETS_PAIR[element[1]] = element[0];
+		OPEN_BRACKETS.push(element[0]);
+	});
+	for (let i = 0; i < str.length; i++) {
+		let currentSymbol = str[i];
+		if (stack[stack.length - 1] == currentSymbol && BRACKETS_PAIR[currentSymbol] == currentSymbol) stack.pop();
+		else if (OPEN_BRACKETS.includes(currentSymbol)) {
+			stack.push(currentSymbol);
+		} else {
+			if (stack.length === 0) {
+				return false;
+			}
+			let topElement = stack[stack.length - 1];
+			if (BRACKETS_PAIR[currentSymbol] === topElement) {
+				stack.pop();
+			} else {
+				return false;
+			}
+		}
+	}
+	return stack.length === 0;
 }
